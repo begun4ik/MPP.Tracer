fb119ddc05d14e22c34426d015838b3108d5d631
Alex_Begun <begun4ik111@mail.ru>
2016-09-26 21:25:37 +0300

    Create project

---
+++ Tracer/Tracer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public sealed class Tracer : ITracer
+    {
+        public TraceResult TraceResult { get; }
+
+        public void StartTrace()
+        {
+        }
+
+        public void StopTrace()
+        {
+        }
+    }
+}
