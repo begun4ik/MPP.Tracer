62bfa4f0d986cbaefaa1374d6f979480c2128bc1
Alex_Begun <begun4ik111@mail.ru>
2016-09-29 22:24:07 +0300

    Add implementation class MethodTrace and ThreadTrace

---
+++ Tracer/Classes/Tracer/MethodTrace.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Classes.Tracer
+{
+    class MethodTrace
+    {
+        public List<MethodTrace> NestedMethods { get; }
+        public TracerInfo.MethodInfo MethodInfo;
+        private Stopwatch _stopwatch;
+
+        internal MethodTrace(MethodBase methodBase)
+        {
+            CreateStopwatch();
+            MethodInfo = new TracerInfo.MethodInfo(methodBase);
+            NestedMethods = new List<MethodTrace>();
+        }
+
+        public void AddNestedMethod(MethodTrace methodTrace)
+        {
+            NestedMethods.Add(methodTrace);
+        }
+
+        public void StopMeteringTime()
+        {
+            _stopwatch.Stop();
+        }
+
+        private void CreateStopwatch()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public long GetExecutionTime()
+        {
+            return _stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
--- Tracer/Classes/Tracer/ThreadTrace.cs
+++ Tracer/Classes/Tracer/ThreadTrace.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tracer.Classes.Tracer;
 
 namespace Tracer.Classes.TracerInfo
 {
     class ThreadTrace
     {
+        private readonly Stack<MethodTrace> _stackMethods;
+        public List<MethodTrace> MethodList { get; }
+        internal ThreadTrace()
+        {
+            _stackMethods = new Stack<MethodTrace>();
+            MethodList = new List<MethodTrace>();
+        }
+
+        public void StartListenMethod(MethodTrace methodTrace)
+        {
+            if(_stackMethods.Count == 0)
+            {
+                MethodList.Add(methodTrace);
+            }
+            else
+            {
+                _stackMethods.Peek().AddNestedMethod(methodTrace);
+            }
+            _stackMethods.Push(methodTrace);
+        }
+        public void StopListenMethod()
+        {
+            _stackMethods.Pop().StopMeteringTime();
+        }
     }
 }
